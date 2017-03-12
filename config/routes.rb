6eec85ebdf4444f930451febd73068a683979274
Mizuki Hidaka <mzkhdk_02@yahoo.co.jp>
2017-03-12 06:26:37 +0000

    create message model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'toilet/top'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -6,7 +8,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-     get 'share_toilet' => 'toilet#top'
+  #   get 'share_toilet' => 'toilet#top'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
